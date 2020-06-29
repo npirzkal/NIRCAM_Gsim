dbfe8958ce119752768dd7cf6fa37e2ed3065b73
Nor Pirzkal <>
2020-06-29 17:28:27 -0400

    Added overwrite=True to background function

--- NIRCAM_Gsim/grism_seed_disperser.py
+++ NIRCAM_Gsim/grism_seed_disperser.py
@@ -150,7 +150,7 @@ class Grism_seed():
             print("Computing dispersed background for order ",order)
             bck += self.this_one[order].disperse_background_1D(background)[self.ystart:self.yend+1,self.xstart:self.xend+1]
 
-        fits.writeto("WFSS_background.fits",bck)
+        fits.writeto("WFSS_background.fits",bck,overwrite=True)
         return bck
 
     def finalize(self,tofits=None,Back=None,BackLevel=None):
