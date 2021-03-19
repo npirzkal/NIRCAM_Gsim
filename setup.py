467af1b69ac25e761b44edfdbe6e7168e5d236b3
Nor Pirzkal <>
2021-03-19 14:30:40 -0400

    1.55

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
 	name='NIRCAM_Gsim',
-	version='1.51',
+	version='1.55',
 	description='NIRCAM Grism simulator, includes J.D. Smith polyclip C code',
     url='https://github.com/npirzkal/GRISM_NIRCAM',
 	author='Nor Pirzkal',
